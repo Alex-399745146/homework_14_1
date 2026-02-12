ced751333d0b484caddf278e64431d989fc4e0d4
Alexsandr Bachevskiy <bachevskiiaa@gmail.com>
2026-02-12 21:53:51 +0300

    check lint_ok, finish work

--- src/my_homework.py
+++ src/my_homework.py
@@ -1,16 +1,19 @@
 # my_homework.py
-""" Модуль содержит описание двух основных классов """
+"""Модуль содержит описание двух основных классов"""
+
 
 # Task_1
 class Product:
-    """ Создание объектов - товаров """
+    """Создание объектов - товаров"""
+
     # Описание типов данных в классе.
     name: str
     description: str
     price: float
     quantity: int
+
     # Конструктор класса с атрибутами объектов.
-    def __init__(self, name, description, price, quantity):
+    def __init__(self, name: str, description: str, price: float, quantity: int) -> None:
         self.name = name
         self.description = description
         self.price = price
@@ -18,18 +21,18 @@ class Product:
 
 
 class Category:
+    category_count: int = 0
+    product_count: int = 0
+
     """ Создание групп товаров """
     name: str
     description: str
     products: list
-    def __init__(self, name, description, products):
+
+    def __init__(self, name: str, description: str, products: list) -> None:
         self.name = name
         self.description = description
         self.products = products
 
-
-
-
-
-
-
+        Category.category_count += 1
+        Category.product_count += len(self.products)
